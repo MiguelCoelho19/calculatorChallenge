a20dd4689af0f7ddd333b29319c44062740de012
Miguel Coelho <80353061+MiguelCoelho19@users.noreply.github.com>
2025-01-16 14:50:56 +0000

    calculator operations

--- calculator/src/main/java/com/challenge/calculator/CalculatorApplication.java
+++ calculator/src/main/java/com/challenge/calculator/CalculatorApplication.java
@@ -2,6 +2,7 @@ package com.challenge.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.text.DecimalFormat;
 
 @SpringBootApplication
 public class CalculatorApplication {
@@ -10,4 +11,35 @@ public class CalculatorApplication {
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
 
+	public String round(double number, int precision) {
+		String pattern = "#";
+		if (precision > 0) {
+			pattern += ".";
+			for (int i = 0; i < precision; i++) {
+				pattern += "#";
+			}
+		}
+
+		DecimalFormat df_obj = new DecimalFormat(pattern);
+		return df_obj.format(number);
+	}
+
+	public String sum(double a, double b, int precision) {
+		return round(a + b, precision);
+	}
+
+	public String subtraction(double a, double b, int precision) {
+		return round(a - b, precision);
+	}
+
+	public String multiplication(double a, double b, int precision) {
+		return round(a * b, precision);
+	}
+
+	public String division(double a, double b, int precision) {
+		if(b==0){
+			throw new ArithmeticException("Cannot divide by zero");
+		}
+		return round(a / b, precision);
+	}
 }
